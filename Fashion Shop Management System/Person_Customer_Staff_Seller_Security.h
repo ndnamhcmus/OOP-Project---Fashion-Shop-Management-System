a65c08d380c2acb162e2fef6d118ffe9407f41e0
wia296 <19127134@student.hcmus.edu.vn>
2020-08-23 14:38:41 +0700

    update security & staff

--- Fashion Shop Management System/Person_Customer_Staff_Seller_Security.h
+++ Fashion Shop Management System/Person_Customer_Staff_Seller_Security.h
@@ -23,17 +23,17 @@ protected:
 
 public:
 
-	Person(string name = "", Date date= Date (0,0,0), string phone = "", Address add= Address("", "", "", "", ""))
+	Person(string name = "", Date dob= Date (0,0,0), string phone = "", Address add= Address("", "", "", "", ""))
 	{
 
 			_name = name;
-			_date_of_birth = date;
+			_date_of_birth = dob;
 			_phone_number = phone;
 			_address = add;
 	};
 	void setName(string _name);
 	void setDoB(Date dob);
-	void setPhoneNumber(int phone);
+	void setPhoneNumber(string phone);
 	void setAddress(Address add);
 	string getName();
 	Date getDoB();
@@ -57,14 +57,16 @@ private:
 
 public:
 
-	Customer(string name = "", Date date = Date(0, 0, 0), string phone = "", Address add = Address("", "", "", "", ""), string customer_id = "")
+	Customer(string name = "", Date dob = Date(0, 0, 0), string phone = "", Address add = Address("", "", "", "", ""), string customer_id = ""): Person(name, dob, phone, add)
+
 	{
 		_customer_id = customer_id;
 	};
-	void setCustomerInfo(string name, Date dob, int phone, Address add, string customer_id);
+	void setCustomerID(string customer_id);
+	void setCustomerInfo(string name, Date dob, string phone, Address add, string customer_id);
+	string getCustomerID();
 	Customer getCustomerInfo();
 	string toString();
-	string getCustomerID();
 	void showCustomerInfo();
 
 	////		Friend		////
@@ -75,27 +77,29 @@ public:
 
 };
 
-/*
-///////////////////////////// Do-ING /////////////////////
+
+
 class Staff : public Person
 {
 protected:
 
 	string _staff_id;
 	double _base_salary = 0;
-
-private:
-
+	void setStaffInfo(string name, Date dob, string phone, Address add, string staff_id, double base_salary);
 	void setStaffID(string staff_id);
 	void setBaseSalary(double salary);
-	void setStaffInfo(string name, Date date, string phone, Address add, string staff_id, double base_salary);
-
+	
 public:
 
-	Staff(string name = "", Date date = Date(0, 0, 0), string phone = "", Address add = Address("", "", "", "", ""), string staff_id = "", double base_salary = 0);
+	Staff(string name = "", Date dob = Date(0, 0, 0), string phone = "", Address add = Address("", "", "", "", ""), string staff_id = "", double base_salary = 0) : Person(name, dob, phone, add)
+	{
+		_staff_id = staff_id;
+		_base_salary = base_salary;
+	}
+
 	string getStaffID();
 	void showStaffInfo();
-	void saveStaffInfoToFile();
+	void virtual saveStaffInfoToFile()=0;
 
 	////  Friend //
 
@@ -106,6 +110,27 @@ public:
 
 
 
+
+class Security : public Staff
+{
+
+public:
+
+	Security(string name = "", Date dob = Date(0, 0, 0), string phone = "", Address add = Address("", "", "", "", ""), string staff_id = "", double base_salary = 0) : Staff(name, dob, phone, add, staff_id, base_salary) {};
+	void setSecurityInfo(string name, Date dob, string phone, Address add, string staff_id, double base_salary);
+	double getSalary();
+	void saveStaffInfoToFile();
+
+	/// Friend ///
+
+	friend class ExcelFstream;
+
+	friend class ExcelIfstream;
+
+	friend class ExcelOfstream;
+};
+
+/*
 class Seller : public Staff
 {
 private:
@@ -131,26 +156,6 @@ public:
 	friend class ExcelOfstream;
 };
 
-
-
-class Security : public Staff
-{
-private:
-
-
-public:
-
-	Security(string name, Date date_of_birth, string phone_number, Address address, string staff_id, double _base_salary) : Staff(name, date_of_birth, phone_number, address, staff_id, _base_salary)
-	{
-
-	}
-
-	friend class ExcelFstream;
-
-	friend class ExcelIfstream;
-
-	friend class ExcelOfstream;
-};
 */
 
 #endif // !_PERSON_CUSTOMER_STAFF_SELLER_SECURITY_
